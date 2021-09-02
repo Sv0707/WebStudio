2f5de323b015fc7473448ef44423a6cd8a7b04d7
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-09-02 15:54:47 +0300

    update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBody: document.querySelector('.backdrop'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBody.addEventListener('click', function (e) {
+    if (e.target == document.querySelector('.backdrop')) {
+      toggleModal();
     }
-  })();
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
